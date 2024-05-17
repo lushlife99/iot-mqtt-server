ae7384bea321d9dfbacc323fa4ceac02f68fb0af
lushlife99 <101994803+lushlife99@users.noreply.github.com>
2024-05-18 00:47:57 +0900

    first commit

---
+++ src/main/java/com/example/mqttserver/MqttserverApplication.java
@@ -0,0 +1,13 @@
+package com.example.mqttserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MqttserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MqttserverApplication.class, args);
+    }
+
+}
